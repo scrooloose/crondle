353af1b18a4223b4617248dae48b1f465b37d00a
Martin Grenfell <martin_grenfell@msn.com>
2008-04-05 16:06:57 +1300

    put the classes in their own Cron module

--- crontab.rb
+++ crontab.rb
@@ -1,105 +1,106 @@
-class CronTabBuilder
+module Cron
+  class Builder
 
-  def desc(desc)
-    @next_desc = desc 
+    def desc(desc)
+      @next_desc = desc 
+    end
+
+    def job(cmd, options)
+      self.jobs << Job.new(next_desc, cmd, options)
+    end
+
+    def daily_job(cmd, hour = 0, minute = 0)
+      self.jobs << Job.new(next_desc, cmd, :hour => hour, :minute => 0)
+    end
+
+    def self.define_jobs(&block)
+      yield instance
+      puts instance.jobs.join("\n\n")
+    end
+
+    def self.instance
+      @instance ||= new
+    end
+
+    def jobs
+      @jobs ||= []
+    end
+
+    private
+    def initialize
+    end
+
+    def next_desc
+      @next_desc || ""
+    end
+
   end
 
-  def job(cmd, options)
-    self.jobs << CronJob.new(next_desc, cmd, options)
-  end
 
-  def daily_job(cmd, hour = 0, minute = 0)
-    self.jobs << CronJob.new(next_desc, cmd, :hour => hour, :minute => 0)
-  end
+  class Job
+    class InvalidTimingParamError < StandardError; end
 
-  def self.define_jobs(&block)
-    yield instance
-    puts instance.jobs.join("\n\n")
-  end
 
-  def self.instance
-    @instance ||= new
-  end
+    attr_reader :minute,
+                :hour,
+                :day_of_month,
+                :month,
+                :day_of_week
 
-  def jobs
-    @jobs ||= []
-  end
+    attr_accessor :description,
+                  :cmd
 
-  private
-  def initialize
-  end
+    def initialize(description, cmd, timing_options)
+      self.description = description
+      self.cmd = cmd
+      set_timing_options(timing_options)
+    end
 
-  def next_desc
-    @next_desc || ""
-  end
+    def set_timing_options(options)
+      self.minute = options[:minute] if options[:minute]
+      self.hour = options[:hour] if options[:hour]
+      self.day_of_month = options[:day_of_month] if options[:day_of_month]
+      self.month = options[:month] if options[:month]
+      self.day_of_week = options[:day_of_week] if options[:day_of_week]
+    end
 
+    def minute=(minute)
+      raise(InvalidTimingParamError, "Minute must be in the range 0..59") unless (0..59).include?(minute)
+      @minute = minute
+    end
+
+    def hour=(hour)
+      raise(InvalidTimingParamError, "Hour must be in the range 0..23") unless (0..23).include?(hour)
+      @hour = hour
+    end
+
+    def day_of_month=(day_of_month)
+      raise(InvalidTimingParamError, "Day of month must be in the range 0..31") unless (0..31).include?(day_of_month)
+      @day_of_month = day_of_month
+    end
+
+    def month=(month)
+      raise(InvalidTimingParamError, "Month must be in the range 1..12") unless (1..12).include?(month)
+      @month = month
+    end
+
+    def day_of_week=(day_of_week)
+      @day_of_week = case day_of_week
+                     when :sunday:    0
+                     when :monday:    1
+                     when :tuesday:   2
+                     when :wednesday: 3
+                     when :thursday:  4
+                     when :friday:    5
+                     when :saturday:  6
+                     else
+                       raise(InvalidTimingParamError, "Day of week must be one of :monday, :tuesday, etc")
+                     end
+    end
+
+    def to_s
+      "# #{description}\n#{minute || "*"} #{hour || "*"} #{day_of_month || "*"} #{month || "*"} #{day_of_week || "*"} #{cmd}"
+    end
+
+  end
 end
-
-
-class CronJob
-  class InvalidTimingParamError < StandardError; end
-
-
-  attr_reader :minute,
-              :hour,
-              :day_of_month,
-              :month,
-              :day_of_week
-
-  attr_accessor :description,
-                :cmd
-
-  def initialize(description, cmd, timing_options)
-    self.description = description
-    self.cmd = cmd
-    set_timing_options(timing_options)
-  end
-
-  def set_timing_options(options)
-    self.minute = options[:minute] if options[:minute]
-    self.hour = options[:hour] if options[:hour]
-    self.day_of_month = options[:day_of_month] if options[:day_of_month]
-    self.month = options[:month] if options[:month]
-    self.day_of_week = options[:day_of_week] if options[:day_of_week]
-  end
-
-  def minute=(minute)
-    raise(InvalidTimingParamError, "Minute must be in the range 0..59") unless (0..59).include?(minute)
-    @minute = minute
-  end
-
-  def hour=(hour)
-    raise(InvalidTimingParamError, "Hour must be in the range 0..23") unless (0..23).include?(hour)
-    @hour = hour
-  end
-
-  def day_of_month=(day_of_month)
-    raise(InvalidTimingParamError, "Day of month must be in the range 0..31") unless (0..31).include?(day_of_month)
-    @day_of_month = day_of_month
-  end
-
-  def month=(month)
-    raise(InvalidTimingParamError, "Month must be in the range 1..12") unless (1..12).include?(month)
-    @month = month
-  end
-
-  def day_of_week=(day_of_week)
-    @day_of_week = case day_of_week
-                   when :sunday:    0
-                   when :monday:    1
-                   when :tuesday:   2
-                   when :wednesday: 3
-                   when :thursday:  4
-                   when :friday:    5
-                   when :saturday:  6
-                   else
-                     raise(InvalidTimingParamError, "Day of week must be one of :monday, :tuesday, etc")
-                   end
-  end
-
-  def to_s
-    "# #{description}\n#{minute || "*"} #{hour || "*"} #{day_of_month || "*"} #{month || "*"} #{day_of_week || "*"} #{cmd}"
-  end
-
-end
-
