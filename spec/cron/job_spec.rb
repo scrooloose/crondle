1d004014689bf70389fa800fb42f64dc4dd1ccb5
Martin Grenfell <martin_grenfell@msn.com>
2008-04-07 20:47:07 +1200

    added some examples for Cron::Job

--- spec/cron/job_spec.rb
+++ spec/cron/job_spec.rb
@@ -2,8 +2,55 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 module Cron
   describe Job do
-    it "should do stuff" do
-      1.should equal(1)
+    it "shouldnt allow 'minute' to be < 0" do
+      lambda {Job.new("desc", "ls", :minute => -1)}.should raise_error(Job::InvalidTimingParamError)
     end
+
+    it "shouldnt allow 'minute' to be > 60" do
+      lambda {Job.new("desc", "ls", :minute => 61)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'hour' to be > 23" do
+      lambda {Job.new("desc", "ls", :hour => 24)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'hour' to be < 0" do
+      lambda {Job.new("desc", "ls", :hour => -1)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'day_of_month' to be > 31" do
+      lambda {Job.new("desc", "ls", :day_of_month => 32)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'day_of_month' to be < 0" do
+      lambda {Job.new("desc", "ls", :day_of_month => -1)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'month' to be < 0" do
+      lambda {Job.new("desc", "ls", :month => -1)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'month' to be > 12" do
+      lambda {Job.new("desc", "ls", :month => 13)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "shouldnt allow 'day_of_week' to be other than :monday||:tuesday||:wednesday||etc" do
+      lambda {Job.new("desc", "ls", :day_of_week => :foobarday)}.should raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "should allow 'day_of_week' to be :monday||:tuesday||:wednesday||etc" do
+      lambda {Job.new("desc", "ls", :day_of_week => :monday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :tuesday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :wednesday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :thursday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :friday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :saturday)}.should_not raise_error(Job::InvalidTimingParamError)
+      lambda {Job.new("desc", "ls", :day_of_week => :sunday)}.should_not raise_error(Job::InvalidTimingParamError)
+    end
+
+    it "should default all timing options to '*' if they arent specified" do
+      Job.new("desc", "ls", {}).to_s.count("*").should equal(5)
+    end
+
   end
 end
